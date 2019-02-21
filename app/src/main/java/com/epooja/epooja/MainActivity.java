04bcde014236a7caf952c1c14cbbd1f11826b588
rohitrainbow <rohitrainbow@yahoo.co.in>
2019-02-21 15:26:41 +0530

    FB Login Integrated

--- app/src/main/java/com/epooja/epooja/MainActivity.java
+++ app/src/main/java/com/epooja/epooja/MainActivity.java
@@ -8,12 +8,22 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import static java.lang.String.*;
+
 public class MainActivity extends AppCompatActivity {
 
     private EditText name;
     private EditText password;
     private TextView info;
     private Button loginButton;
+    private LoginButton fbLoginButton;
+    private CallbackManager callbackManager;
     private int counter = 5;
 
     @Override
@@ -21,11 +31,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
         name = findViewById(R.id.etName);
         password = findViewById(R.id.etPassword);
         info = findViewById(R.id.tvInfo);
         loginButton = findViewById(R.id.btnLogin);
-        info.setText(getText(R.string.atmpt_rem)+""+counter);
+        fbLoginButton=findViewById(R.id.btnFacebookLogin);
+        callbackManager=CallbackManager.Factory.create();
+        info.setText(format("%s%d", getText(R.string.atmpt_rem), counter));
+
+
+        fbLoginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onCancel() {
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,7 +78,13 @@ public class MainActivity extends AppCompatActivity {
             if (counter == 0) {
                 loginButton.setEnabled(false);
             }
-            info.setText(getText(R.string.atmpt_rem)+""+counter);
+            info.setText(format("%s%d", getText(R.string.atmpt_rem), counter));
         }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        callbackManager.onActivityResult(requestCode,resultCode,data);
     }
 }
